bc5be1e3b986e185c642fd553e0db5c0fdd60284
DONG <dongchuan55@gmail.com>
2016-06-12 21:51:51 +0200

    Init project

---
+++ flexibleripplebackground/src/main/java/io/github/loop_x/flexibleripplebackground/FlexibleRippleBackgroundView.java
@@ -0,0 +1,20 @@
+package io.github.loop_x.flexibleripplebackground;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FlexibleRippleBackgroundView extends View {
+
+    public FlexibleRippleBackgroundView(Context context) {
+        super(context);
+    }
+
+    public FlexibleRippleBackgroundView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FlexibleRippleBackgroundView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
